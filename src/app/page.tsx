1097bc3a3b550a44958f9ee00ca6ed5a6951c10b
Tengis S <tengis.s@gmobile.mn>
2024-04-13 16:48:42 +0800

    json update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-gray-900 p-6 ">
       <div className=" w-1/3 lg:w-1/5 h-full shadow-lg shadow-gray-400 bg-gray-800  ">
-        <div className="flex flex-col justify-center h-2/5 items-center">
+        <div className="flex flex-col justify-center h-1/3 items-center">
           <div className="w-1/2 sm:w-[150px] px-4">
             <div className="border-4 border-blue-500 rounded-full">
               <Image
@@ -27,11 +27,44 @@ export default function Home() {
           <Icons />
         </div>
         <div className="flex flex-col justify-center h-3/5 items-center">
-          <div className="w-full h-2/5 flex justify-center flex-wrap">
+          <div className="w-full h-[350px] flex flex-wrap">
             <h3 className="text-white font-sans.... text-lg w-full text-center">
               Frontend Developer
             </h3>
             {frontend?.map((data, index) => {
+              return (
+                <div className="w-1/2 h-auto flex-col flex">
+                  <div className="flex flex-row items-center w-full ml-2">
+                    <svg
+                      className="h-4 w-4 text-blue-400"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      {" "}
+                      <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                      <circle cx="12" cy="12" r="9" />{" "}
+                      <path d="M9 12l2 2l4 -4" />
+                    </svg>
+                    <h3 className="text-white ml-2 text-sm">{data.name}</h3>
+                  </div>
+                  <div className="w-full flex pl-8 text-gray-400 text-xs">
+                    {data.level}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="w-full h-[350px] flex justify-center flex-wrap">
+            <h3 className="text-white font-sans.... text-lg w-full text-center">
+              Backend Developer
+            </h3>
+            {backend?.map((data, index) => {
               return (
                 <div className="w-1/2 h-auto">
                   <div className="flex flex-row items-center w-full ml-2">
@@ -53,15 +86,14 @@ export default function Home() {
                     </svg>
                     <h3 className="text-white ml-2 text-sm">{data.name}</h3>
                   </div>
-                  <div className="w-full flex justify-center text-gray-400 text-xs">
+                  <div className="w-full flex pl-8 text-gray-400 text-xs">
                     {data.level}
                   </div>
                 </div>
               );
             })}
           </div>
-          <div className="w-full h-2/5 bg-red-300"></div>
-          <div className="w-full h-1/5 bg-red-200"></div>
+          <div className="w-full h-auto bg-red-200"></div>
         </div>
         <div></div>
       </div>
